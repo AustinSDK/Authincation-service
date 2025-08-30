b3db98bbb4dcc984451aec6b62f2529fce2479b6
Austin Moore <133458852+Null-Austin@users.noreply.github.com>
2025-08-30 08:57:32 -0500

    added dynamic file paths + ejs :D

--- src/app.js
+++ src/app.js
@@ -1,17 +1,39 @@
-require("dotenv").config()
+require("dotenv").config();
+
+const path = require("path");
+const fs = require("fs")
 
 const express = require("express");
 const app = express();
+app.set("views", path.join(__dirname, "pages"));
+app.set("view engine", "ejs");
 
+// dynamic file paths
+
+// css paths
+app.get("/css/:path",(req,res,next)=>{
+    // get path
+    const css_path = path.join(__dirname,'css')
+    const _path = path.join(css_path,req.params.path)
+
+    // check path to make sure its real
+    if (!_path.startsWith(css_path)) return next()
+
+    // send file
+    if (!fs.existsSync(_path)) return next();
+    res.sendFile(_path)
+});
+
+// Static file paths
 app.get("/",(req,res)=>{
-    res.send("hii")
-})
+    res.render("index.ejs");
+});
 
 const port = process.env.port
 app.listen(port,e=>{
     if (e){
-        console.error(e)
-        return
+        console.error(e);
+        return;
     }
-    console.log("hosting on port "+port)
-})
+    console.log("hosting on port "+port);
+});
